c54461548c15fddecda777f50e1c4c7ccc8d0aad
J Odhiambo <121058318+4ir-jojo@users.noreply.github.com>
2024-02-23 22:50:05 +0300

    Add files via upload

---
+++ user_input.py
@@ -0,0 +1,7 @@
+#this my user input
+name = input(":")
+age = input(":")
+location = input(":")
+print(f"Raw input values: name='{name}', age='{age}', location='{location}'")
+
+print (f"Hello {name}, you are {age} years old and live in {location}.")
